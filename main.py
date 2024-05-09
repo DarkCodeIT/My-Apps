00621dfbb61a40c1ab7b823dc18484e6e23d5e79
DarkCodeIT <maksplay298@gmail.com>
2024-05-09 16:08:58 +0500

    Better UI

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 from flet import *
 
 
-class InputField(UserControl):
-    def __init__(self, width: int, height: int, hint_text: str, icon):
+class InputField(Stack):
+    def __init__(self, width: int = 0, height: int = 0, hint_text: str = "", icon: icons = None, password: bool = False):
         super().__init__()
         self.width = width
         self.height = height
         self.hint_text = hint_text
+        self.password = password
         self.icon = icon
 
         self.field = Container(
             Row([
                 TextField(
                     hint_text=self.hint_text,
-                    border=None,
-                    bgcolor = "transparent"
-                ),
-                self.icon
-            ]),
+                    border=InputBorder.NONE,
+                    suffix_icon=self.icon,
+                    bgcolor = "transparent",
+                    color = "white",
+                    filled=False,
+                    password=self.password,
+                    cursor_color=colors.CYAN_ACCENT_200,
+                    hint_style=TextStyle(
+                        color="blue"
+                    ))
+                ],
+                alignment=MainAxisAlignment.SPACE_AROUND),
             width=self.width,
             height=self.height,
             bgcolor="transparent",
-            border=border.all(1, "blue")
+            border=border.all(1, colors.BLUE_400),
+            border_radius=20
         )
 
-
-
-    def build(self):
+    def build(self) -> Container:
         return self.field
 
 
@@ -47,15 +54,34 @@ def main(page: Page):
                 Column([
                     Text(
                         value="Login",
-                        size=24,
-                        weight=FontWeight.BOLD
+                        size=30,
+                        weight=FontWeight.W_500,
+                        color= colors.WHITE70
+                    ),
+
+                    InputField(
+                        width=350,
+                        height=50,
+                        hint_text="username | email",
+                        icon=icons.PERSON_ROUNDED
+                    ),
+
+                    InputField(
+                        width=350,
+                        height=50,
+                        hint_text="password",
+                        password=True,
+                        icon=icons.LOCK_ROUNDED
                     )
-                ], horizontal_alignment="center"),
+                ],
+                    horizontal_alignment="center",
+                    alignment="center",
+                    spacing=20),
 
                 width=400,
-                height=500,
+                height=400,
                 border_radius=20,
-                border=border.all(1,"blue"),
+                border=border.all(1, colors.CYAN_ACCENT_400),
                 bgcolor="transparent",
                 blur=Blur(10,10 ,BlurTileMode.MIRROR)
 
