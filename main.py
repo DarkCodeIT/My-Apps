bc262320a1d08e4f66fb2bfd555b7569958a039e
DarkCodeIT <maksplay298@gmail.com>
2024-05-09 13:44:25 +0500

    FrontEnd for Login page

--- main.py
+++ main.py
@@ -1,40 +1,74 @@
-import flet as ft
+from flet import *
 
 
-def main(page: ft.Page):
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.window_width = 350
-    page.window_height = 650
+class InputField(UserControl):
+    def __init__(self, width: int, height: int, hint_text: str, icon):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.hint_text = hint_text
+        self.icon = icon
 
-    first_name = ft.TextField(label="First name", width=225, autofocus=True)
-    last_name = ft.TextField(label="Last name", width=225)
-    email = ft.TextField(label="Email", width=225)
-    password = ft.TextField(label="Password", width=225, password=True, can_reveal_password=True)
+        self.field = Container(
+            Row([
+                TextField(
+                    hint_text=self.hint_text,
+                    border=None,
+                    bgcolor = "transparent"
+                ),
+                self.icon
+            ]),
+            width=self.width,
+            height=self.height,
+            bgcolor="transparent",
+            border=border.all(1, "blue")
+        )
 
-    view = ft.Container(content=ft.Column(controls=[
-        first_name,
-        last_name,
-        email,
-        password
-    ],
-    alignment=ft.MainAxisAlignment.CENTER),
-    width=300,
-    height=400,
-    gradient=ft.LinearGradient(
-        begin=ft.alignment.top_center,
-        end=ft.alignment.bottom_center,
-        colors=[ft.colors.DEEP_PURPLE_50, ft.colors.AMBER_100]
-    ),
-    alignment=ft.alignment.center,
-    border_radius=35,
-    )
+
+
+    def build(self):
+        return self.field
+
+
+def main(page: Page):
+    # page.window_width = 420
+    # page.window_height = 640
+    page.window_resizable = False
+    page.padding = 0
+    page.vertical_alignment = "center"
+    page.horizontal_alignment = "center"
+
+    body = Stack([
+        Image(
+            src="assets/bg_login.jpg"
+        ),
+        Container(
+            Container(
+                Column([
+                    Text(
+                        value="Login",
+                        size=24,
+                        weight=FontWeight.BOLD
+                    )
+                ], horizontal_alignment="center"),
+
+                width=400,
+                height=500,
+                border_radius=20,
+                border=border.all(1,"blue"),
+                bgcolor="transparent",
+                blur=Blur(10,10 ,BlurTileMode.MIRROR)
+
+            ),
+            alignment=alignment.center,
+            padding=padding.only(top=100)
+        )
+    ])
 
     page.add(
-        view
+        body
     )
-    page.update()
 
 
 if __name__ == "__main__":
-    ft.app(target=main)
+    app(target=main)
